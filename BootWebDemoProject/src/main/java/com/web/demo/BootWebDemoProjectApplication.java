628bc590e5570f77cbe25399bed221bf364c3830
SabrinaSusmi <sabrina.su512@gmail.com>
2021-02-25 16:03:58 +0600

    Project setup
    This reverts commit 8143e215f35596506d6777382f89f5b4393a1e2c.

--- BootWebDemoProject/src/main/java/com/web/demo/BootWebDemoProjectApplication.java
+++ BootWebDemoProject/src/main/java/com/web/demo/BootWebDemoProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootWebDemoApplication {
+public class BootWebDemoProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootWebDemoApplication.class, args);
+		SpringApplication.run(BootWebDemoProjectApplication.class, args);
 	}
 
 }
